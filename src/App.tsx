b7008be20d8945c0f0cc158ae906121f4759458e
[dyad] <git@dyad.sh>
2026-01-13 05:19:51 +0100

    [dyad] Usunięto tymczasowy komponent do tworzenia administratora - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ import ProfilePage from "./pages/ProfilePage";
 import ReportsPage from "./pages/ReportsPage";
 import DivisionsPage from "./pages/DivisionsPage";
 import AnnouncementsPage from "./pages/AnnouncementsPage";
-import NotesPage from "./pages/NotesPage"; // Import NotesPage
+import NotesPage from "./pages/NotesPage";
 import { AuthProvider, ProtectedRoute } from "./contexts/AuthContext";
-import CreateAdminUser from "./components/CreateAdminUser";
+// import CreateAdminUser from "./components/CreateAdminUser"; // Usunięto import
 
 const queryClient = new QueryClient();
 
@@ -27,7 +27,7 @@ const App = () => (
       <BrowserRouter>
         <AuthProvider>
           {/* Temporary component to create admin user - REMOVE AFTER USE */}
-          <CreateAdminUser /> 
+          {/* <CreateAdminUser /> */} {/* Usunięto komponent */}
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
@@ -42,7 +42,7 @@ const App = () => (
                       <Route path="/reports" element={<ReportsPage />} />
                       <Route path="/divisions" element={<DivisionsPage />} />
                       <Route path="/announcements" element={<AnnouncementsPage />} />
-                      <Route path="/notes" element={<NotesPage />} /> {/* Add NotesPage route */}
+                      <Route path="/notes" element={<NotesPage />} />
                       <Route
                         path="/account-management"
                         element={<ProtectedRoute allowedRoles={["Lieutenant", "Captain", "High Command"]}><AccountManagementPage /></ProtectedRoute>}
